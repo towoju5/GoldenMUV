519ed1fd16746e027daf6d3b35a240d116b097d8
EMMANUEL TOWOJU <towojuads@gmail.com>
2023-12-13 03:31:25 +0100

    dashboard page completed

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,20 @@
+// dropdown button
+$(document).ready(function () {
+    $(".dropdown-button").on("click", function () {
+        // find closest tr and toggle dropdown
+        $(this).closest('tr').find('.dropdown-menu').toggleClass("hidden");
+    });
+
+    // Close the dropdowns when clicking outside
+    $(document).on("click", function (event) {
+        if (!$(".dropdown-button").is(event.target) && !$(".dropdown-menu").is(event.target) &&
+            $(".dropdown-menu").has(event.target).length === 0) {
+            $(".dropdown-menu").addClass("hidden");
+        }
+    });
+});
+
+
 // Year at the bottom
 document.getElementById('year').innerHTML = new Date().getFullYear();
+
